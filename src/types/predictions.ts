bfb38522a28e07ce802c8a76138fd31427467f52
Jake Robins <me@jakerobins.com>
2023-05-25 08:46:56 -0600

    add types for API

---
+++ src/types/predictions.ts
@@ -0,0 +1,58 @@
+import { APIResponse } from "./api";
+
+export enum PredictionLifeCycle {
+  OPEN = "open",
+  RETIRED = "retired",
+  CLOSED = "closed",
+  SUCCESSFUL = "successful",
+  FAILED = "failed",
+}
+
+export namespace APIPredictions {
+  export type EnhancedPrediction = {
+    id: number;
+    predictor: {
+      id: string;
+      discord_id: string;
+    };
+    text: string;
+    created_date: string;
+    due_date: string;
+    closed_date: string | null;
+    triggered_date: string | null;
+    triggerer: {
+      id: string;
+      discord_id: string;
+    } | null;
+    judged_date: string | null;
+    retired_date: string | null;
+    status: PredictionLifeCycle;
+    bets: {
+      id: string;
+      endorsed: boolean;
+      date: string;
+      wager: number;
+      valid: boolean;
+      payout: number;
+      better: {
+        id: string;
+        discord_id: string;
+      };
+    }[];
+    votes: {
+      id: string;
+      vote: boolean;
+      voted_date: string;
+      voter: {
+        id: string;
+        discord_id: string;
+      };
+    }[];
+    payouts: {
+      endorse: number;
+      undorse: number;
+    };
+  };
+
+  export type GetPredictionById = APIResponse<EnhancedPrediction>;
+}
--- src/utils/ndb2.ts
+++ src/utils/ndb2.ts
@@ -1,3 +1,6 @@
+import { APIPredictions } from "@/types/predictions";
+import { APIScores, LeaderboardType } from "@/types/scores";
+
 const API_URL = process.env.NDB2_API_BASEURL;
 const API_KEY = process.env.NDB2_API_KEY;
 
@@ -12,13 +15,43 @@ export class Ndb2Client {
     };
   }
 
-  public getLeaderboard(
-    type: "points" | "predictions" | "bets",
+  public getPointsLeaderboard(
     headers: RequestInit["headers"] = {}
-  ) {
+  ): Promise<APIScores.GetPointsLeaderboard> {
     const reqHeaders = new Headers({ ...this.headers, ...headers });
 
-    return fetch(this.baseUrl + `/api/scores?view=${type}`, {
+    return fetch(this.baseUrl + `/api/scores?view=points`, {
+      headers: reqHeaders,
+    }).then((res) => res.json());
+  }
+
+  public getBetsLeaderboard(
+    headers: RequestInit["headers"] = {}
+  ): Promise<APIScores.GetBetsLeaderboard> {
+    const reqHeaders = new Headers({ ...this.headers, ...headers });
+
+    return fetch(this.baseUrl + `/api/scores?view=bets`, {
+      headers: reqHeaders,
+    }).then((res) => res.json());
+  }
+
+  public getPredictionsLeaderboard(
+    headers: RequestInit["headers"] = {}
+  ): Promise<APIScores.GetPredictionsLeaderboard> {
+    const reqHeaders = new Headers({ ...this.headers, ...headers });
+
+    return fetch(this.baseUrl + `/api/scores?view=predictions`, {
+      headers: reqHeaders,
+    }).then((res) => res.json());
+  }
+
+  public getPredictionById(
+    id: Number,
+    headers: RequestInit["headers"] = {}
+  ): Promise<APIPredictions.GetPredictionById> {
+    const reqHeaders = new Headers({ ...this.headers, ...headers });
+
+    return fetch(this.baseUrl + `/api/predictions/${id}`, {
       headers: reqHeaders,
     }).then((res) => res.json());
   }
