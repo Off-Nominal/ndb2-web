dbf5dc13e373bf5558395883ab9fb3f0cf0402a4
David Halpin <devhalpin86@gmail.com>
2023-06-06 14:22:23 -0700

    update Triangle with className prop

--- src/components/Triangle.tsx
+++ src/components/Triangle.tsx
@@ -2,21 +2,23 @@ type TriangleProps = {
   fillColor?: string,
   strokeColor?: string,
   hoverColor?: string,
-  onClick: () => void
+  onClick: () => void,
+  className?: string,
+  
 }
 
 export const Triangle = (props:TriangleProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 200 40"
-      className="relative w-40 h-20"
+      viewBox="0 0 10 10"
+      className={`w-10 h-10 ${props.className}`}
     >
       <polygon
-        points="110,10 130,50 90,50"
+        points="5,0 0,10 10,10"
         fill={props.fillColor? props.fillColor : "white"}
         stroke={props.strokeColor? props.strokeColor : "black"}
-        strokeWidth="1"
+        strokeWidth="0.25"
         className={`hover:fill-${props.hoverColor? props.hoverColor : "moss-green"}`}
         onClick={()=> props.onClick()}
       />
