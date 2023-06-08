fb946b0385ce6c84d4cd22475291693c7a006cbb
David Halpin <devhalpin86@gmail.com>
2023-06-07 20:03:47 -0700

    remove border around PillDisplay

--- src/components/PillDisplay.tsx
+++ src/components/PillDisplay.tsx
@@ -1,6 +1,6 @@
 export const PillDisplay = (props: {text: string, color?: string}) => {
   return (
-    <div className={`relative flex flex-wrap content-center justify-center h-auto rounded-full w-36 border ${props.color ? props.color : "bg-moonstone-blue"}`}>
+    <div className={`relative flex flex-wrap content-center justify-center h-auto rounded-full w-32 ${props.color ? props.color : "bg-moonstone-blue"}`}>
       <p>{props.text}</p>
     </div>
   );
